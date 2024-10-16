dc4a41657b403e71bd373ec3c5a59ef066606a33
Aditi Mishra <aditimishra91924@gmail.com>
2024-10-16 18:07:50 +0530

    Enhanced SuperContraGame , MarioGame , Pacman , GameRunner  class with @Component, @Primary, and @Qualifier annotations

--- src/main/java/com/Spring/Learn_Spring_Framework/game/PacmanGame.java
+++ src/main/java/com/Spring/Learn_Spring_Framework/game/PacmanGame.java
@@ -1,5 +1,8 @@
 package com.Spring.Learn_Spring_Framework.game;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class PacmanGame implements GamingConsole{
     public void up(){
         System.out.println("UP");
--- src/main/java/com/Spring/Learn_Spring_Framework/game/SuperContraGame.java
+++ src/main/java/com/Spring/Learn_Spring_Framework/game/SuperContraGame.java
@@ -1,5 +1,10 @@
 package com.Spring.Learn_Spring_Framework.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("SuperContraQualifier")
 public class SuperContraGame implements GamingConsole{
     public void up(){
         System.out.println("Up");
